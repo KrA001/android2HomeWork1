492f5d8373b15194df14fbbbe6cb33aa968f9306
LENOVO <test23062000@gmail.com>
2023-12-31 08:46:37 +0300

    dz

--- app/src/main/java/com/example/android2homework1/MainActivity.kt
+++ app/src/main/java/com/example/android2homework1/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.android2homework1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.android2homework1.databinding.ActivityMainBinding
